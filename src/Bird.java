6a5d4ecdf15034dbfc88a0f5e3e5cfa1e6f6e393
Dominic Bou-Samra <domlebo70@Dominics-MacBook-Air.local>
2012-05-18 08:31:30 +1000

    Initial commit

---
+++ src/Bird.java
@@ -0,0 +1,72 @@
+import java.util.Random;
+
+
+public class Bird {
+
+	double reserves = 0;
+	int points = 0;
+	boolean hasMate = false;
+	
+	
+	public Bird() {
+		
+	}
+	
+	private void sing() {
+		double foodUsed = 12 * (0.002 * this.reserves) + getB();
+		this.reserves += foodUsed;
+		pairWithMate();
+	}
+	
+	private void forage() {
+		double foodUsed = 8 + (0.007 * this.reserves) + getB();
+		this.reserves += foodUsed;
+		findFood();
+	}
+	
+	private void rest() {
+		this.reserves += -3.6;
+	}
+	
+	public boolean isDead() {
+		return this.reserves <= 0;
+	}
+	
+	private void pairWithMate() {
+		//0.004 % chance to mate
+		if (new Random().nextInt(1000) > 4) {
+			this.hasMate = true;
+		}
+	}
+	
+	private void findFood() {
+		if( new Random().nextInt(100) < 60) {
+			this.reserves += 32;
+		}
+	}
+	
+	private void calculatePoints() {
+		if (hasMate && !isDead()) {
+			points += 2;
+		}
+		else if (!isDead()) {
+			points += 1;
+		}
+		else if (isDead()) {
+			points += 0;
+		}
+	}
+
+	private double getB() {
+		int randomNumber = new Random().nextInt(100);
+		if (randomNumber < 50) {
+			return -6.4;
+		}
+		else if (randomNumber >= 50 && randomNumber < 75) {
+			return 0;
+		}
+		else {
+			return 6.4;
+		}
+	}
+}
---
+++ src/Environment.java
@@ -0,0 +1,42 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+
+public class Environment {
+	
+	final int DUSK = 74;
+	
+	int time = 0;
+	
+	public Environment() { }
+
+	public static void main(String[] args) {
+		Environment env = new Environment();
+		Bird bird = new Bird();
+		
+		double x = bird.reserves + 32 - 
+		double p = x - Math.abs(x);
+		double nextReserves = bird.reserves;
+		
+		Random rand = new Random();
+		
+		if (rand.nextInt() < p) {
+			
+		}
+		
+		while (!bird.isDead()) {
+			System.out.println("NOT DEAD");
+		}
+	}
+	
+	private void increaseTime() {
+		this.time += 1;
+	}
+	
+	private boolean isDay() {
+		return time < DUSK;
+	}
+	
+
+}
