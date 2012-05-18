656b60b1ebc29244c79527c1dd11301e9baf5f51
db5732 <db5732@t9db5732.(none)>
2012-05-18 16:18:22 +1000

    First working example

--- src/Environment.java
+++ src/Environment.java
@@ -15,21 +15,38 @@ public class Environment {
 		Environment env = new Environment();
 		Bird bird = new Bird();
 		
-		double x = bird.reserves + 32 - 
-		double p = x - Math.abs(x);
-		double nextReserves = bird.reserves;
-		
-		Random rand = new Random();
-		
-		if (rand.nextInt() < p) {
-			
+		for (int i = 0; i < 150; i++) {
+			if (bird.isDead()) {
+				break;
+			}
+			else {
+				env.calculateReserves(bird);
+				System.out.println("Reserves = " + bird.reserves);
+			}
 		}
-		
-		while (!bird.isDead()) {
-			System.out.println("NOT DEAD");
-		}
+		System.out.println(bird.points);
 	}
 	
+	private void calculateReserves(Bird bird) {
+		
+		if (isDay()) {
+			if (bird.reserves >= 32) {
+				bird.sing();
+				System.out.println("Sing");
+			}
+			else {
+				bird.forage();
+				System.out.println("Forage");
+			}
+		}
+		else {
+			bird.rest();
+			System.out.println("Rest");
+		}
+		bird.calculatePoints();
+		
+	}
+
 	private void increaseTime() {
 		this.time += 1;
 	}
